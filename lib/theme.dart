d26306f054275b8c87361ab7d8cdb0fe12822af7
s4llm4n <55424625+s4llm4n@users.noreply.github.com>
2023-02-28 12:02:02 +0700

    commit main

--- lib/theme.dart
+++ lib/theme.dart
@@ -9,6 +9,8 @@ Color alertColor = Color(0xffEd6363);
 Color priceColor = Color(0xff2C96F1);
 Color backgroundColor1 = Color(0xff1F1D2B);
 Color backgroundColor2 = Color(0xff2B2937);
+Color backgroundColor3 = Color(0xff242231);
+Color backgroundColor4 = Color(0xff252836);
 Color primaryColor3 = Color(0xff242231);
 Color primaryTextColor = Color(0xffF1F0F2);
 Color secondaryTextColor = Color(0xff999999);
@@ -28,6 +30,10 @@ TextStyle subtitleTextStyle = GoogleFonts.poppins(
 
 TextStyle priceTextStyle = GoogleFonts.poppins(
   color: priceColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
 );
 
 FontWeight light = FontWeight.w300;
